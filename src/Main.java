76d68a9e2313eac1fd90495b9bf243d0e6126b04
Guilherme Silva <guiguisilva1104@gmail.com>
2023-09-19 08:25:50 -0300

    validação

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         // Classe Objeto         Contrutor
@@ -7,5 +9,20 @@ public class Main {
         mochileiro.setCpf(Validacao.cpf("123"));
         Turista mochileira = new Turista();
         mochileira.setNome("Tomas");
+        // Atribuição indireta
+        String nome = JOptionPane.showInputDialog("Entre com o nome");
+        mochileira.setNome(nome);
+        System.out.println("O nome do objeto é :" + mochileira.getNome());
+        // Fazer o mesmo para capturar o cpf deste objeto
+        String cpf = JOptionPane.showInputDialog("Entre com o cpf");
+        mochileira.setCpf(cpf);
+        System.out.println("O CPF do objeto é :" + mochileira.getCpf());
+        // Novo objeto
+        nome = JOptionPane.showInputDialog("Qual seu nome: ");
+        // Criando objeto com construtor recebendo nome
+        Turista outroTurista = new Turista(nome);
+        System.out.println("O nome deste turista é :" + outroTurista.getNome());
+        // Fazer o recebimento  do cpf para este turista
+
     }
 }
